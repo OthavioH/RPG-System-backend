bed60a8f54f650e3c69230c0a88b035254d2d64f
Othavio Henrico <othavio2650@gmail.com>
2021-12-05 12:33:09 -0300

    Initiating project

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import cors from 'cors';
+import express from 'express';
+import morgan from 'morgan';
+
+import routes from './routes';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(morgan('dev'));
+
+app.use(routes);
+app.listen(3333);
