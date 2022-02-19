0dd68e8a13e8fc938484260c9be60d3813c1e3a3
Othavio Henrico <46772924+OthavioH@users.noreply.github.com>
2022-02-19 06:18:48 -0300

    Update db.ts

--- src/config/db.ts
+++ src/config/db.ts
@@ -2,5 +2,5 @@ import { Sequelize } from 'sequelize';
 
 const port:number = process.env.DB_PORT as any;
 
-export const db = new Sequelize(process.env.DB_NAME,process.env.DB_USERNAME,process.env.DB_PASSWORD,{dialect:'mysql',host:process.env.DB_HOST,port:(port)});
+export const db = new Sequelize(process.env.DB_NAME,process.env.DB_USERNAME,process.env.DB_PASSWORD,{dialect:'mysql',host:process.env.DB_HOST},);
 
