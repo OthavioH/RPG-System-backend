a1c2969b55956964828fd3f88ba2aee7fdf52241
Othavio Henrico <46772924+OthavioH@users.noreply.github.com>
2022-02-20 12:51:21 -0300

    Update routes.ts

--- src/routers/routes.ts
+++ src/routers/routes.ts
@@ -14,6 +14,7 @@ routes.get('/gamesettings', gameSettingsController.getGameSettings);
 routes.post('/gamesettings/save/properties', gameSettingsController.saveSkillsAndAttributes);
 routes.post('/gamesettings/save/timers', gameSettingsController.saveTimers);
 
+routes.get('/sheets/create', sheetController.createSheet);
 routes.get('/sheets', sheetController.getAll);
 routes.get('/sheets/:id', sheetController.getSheetById);
 routes.get('/sheets/:id/status/update', sheetController.updateHpAndSanity);
