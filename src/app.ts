3fb708c508a6f7617f30e0140aca319607486dff
Othavio Henrico <46772924+OthavioH@users.noreply.github.com>
2022-02-20 12:57:52 -0300

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,7 @@ import {db} from './config/db';
 
 const app = express();
 
-app.use(cors());
+app.use(cors({origin:'null'}));
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(morgan('dev'));
