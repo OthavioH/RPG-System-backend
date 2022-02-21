c84d3748610a98eaef20e0351774879914fe7198
Othavio Henrico <46772924+OthavioH@users.noreply.github.com>
2022-02-20 22:36:20 -0300

    Update routes.ts

--- src/routers/routes.ts
+++ src/routers/routes.ts
@@ -10,13 +10,13 @@ routes.get('/', (req,res)=>{
 });
 
 routes.get('/gamesettings/create', gameSettingsController.createGameSettings);
-routes.get('/gamesettings', gameSettingsController.getGameSettings);
 routes.post('/gamesettings/save/properties', gameSettingsController.saveSkillsAndAttributes);
 routes.post('/gamesettings/save/timers', gameSettingsController.saveTimers);
+routes.get('/gamesettings', gameSettingsController.getGameSettings);
 
 routes.get('/sheets/create', sheetController.createSheet);
-routes.get('/sheets', sheetController.getAll);
 routes.get('/sheets/:id', sheetController.getSheetById);
 routes.get('/sheets/:id/status/update', sheetController.updateHpAndSanity);
 routes.get('/sheets/:id/update', sheetController.updateOne);
+routes.get('/sheets', sheetController.getAll);
 export default routes;
