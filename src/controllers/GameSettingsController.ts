d70bb6afb2886f3f074408a88e73a5e7aef0445a
Othavio Henrico <46772924+OthavioH@users.noreply.github.com>
2022-03-08 23:10:51 -0300

    Update GameSettingsController.ts

--- src/controllers/GameSettingsController.ts
+++ src/controllers/GameSettingsController.ts
@@ -5,7 +5,8 @@ import { GameSettings } from '../models/GameSettings';
 
 export const gameSettingsController = {
     async createGameSettings(req:Request,res:Response) {
-        const {id} = req.query;
+        const id = req.query["id"];
+        console.log(req.query);
 
         const gameSettings = await GameSettings.create({id:id}).catch((err)=>{
             console.error(err);
