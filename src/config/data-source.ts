c98f870487c910855ab99da037bac9c0d509757d
Othavio Henrico <46772924+OthavioH@users.noreply.github.com>
2022-08-23 20:22:35 -0300

    Added threat

--- src/config/data-source.ts
+++ src/config/data-source.ts
@@ -2,17 +2,18 @@ import "reflect-metadata";
 import { DataSource } from "typeorm";
 import { GameSettings } from "../entity/GameSettings";
 import { Sheet } from "../entity/Sheet";
+import { Threat } from "../entity/Threat";
 
 export const AppDataSource = new DataSource({
-    type:'mysql',
-    host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT),
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    synchronize: true,
-    logging: false,
-    entities: [Sheet, GameSettings],
-    subscribers: [],
-    migrations: [],
+  type: "mysql",
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  synchronize: true,
+  logging: false,
+  entities: [Sheet, GameSettings, Threat],
+  subscribers: [],
+  migrations: [],
 });
--- src/routers/routes.ts
+++ src/routers/routes.ts
@@ -1,28 +1,39 @@
-import express, { Router } from 'express';
+import express, { Router } from "express";
 
-import { GameSettingsController } from '../controllers/GameSettingsController';
-import { SheetController } from '../controllers/SheetController';
+import { GameSettingsController } from "../controllers/GameSettingsController";
+import { SheetController } from "../controllers/SheetController";
+import { ThreatController } from "../controllers/ThreatController";
 
 const routes = express.Router();
 
-routes.get('/', (req, res) => {
-    return res.send('Olá');
+routes.get("/", (req, res) => {
+  return res.send("Olá");
 });
 
-routes.get('/gamesettings/create', GameSettingsController.createGameSettings);
-routes.get('/gamesettings', GameSettingsController.getGameSettings);
+routes.get("/gamesettings/create", GameSettingsController.createGameSettings);
+routes.get("/gamesettings", GameSettingsController.getGameSettings);
 
-routes.post('/gamesettings/properties/save', GameSettingsController.updateGameProperties);
-routes.post('/gamesettings/timers/save', GameSettingsController.saveTimers);
-routes.post('/gamesettings/roll/save', GameSettingsController.addNewRoll);
+routes.post(
+  "/gamesettings/properties/save",
+  GameSettingsController.updateGameProperties
+);
+routes.post("/gamesettings/timers/save", GameSettingsController.saveTimers);
+routes.post("/gamesettings/roll/save", GameSettingsController.addNewRoll);
 
-routes.post('/sheets/create', SheetController.createSheet);
+routes.post("/sheets/create", SheetController.createSheet);
 
-routes.put('/sheets/:id/hp/update', SheetController.updateHp);
-routes.put('/sheets/:id/sanity/update', SheetController.updateSanity);
-routes.put('/sheets/:id/update', SheetController.updateSheet);
-routes.put('/sheets/:id/delete', SheetController.deleteSheet);
+routes.put("/sheets/:id/hp/update", SheetController.updateHp);
+routes.put("/sheets/:id/sanity/update", SheetController.updateSanity);
+routes.put("/sheets/:id/update", SheetController.updateSheet);
+routes.put("/sheets/:id/delete", SheetController.deleteSheet);
 
-routes.get('/sheets/:id', SheetController.getSheet);
-routes.get('/sheets', SheetController.getAllSheets);
+routes.get("/sheets/:id", SheetController.getSheet);
+routes.get("/sheets", SheetController.getAllSheets);
+
+routes.post("/threats/create", ThreatController.createThreat);
+routes.put("/threats/:id/update", ThreatController.updateThreat);
+routes.delete("/threats/:id/delete", ThreatController.deleteThreat);
+routes.get("/threats/:id", ThreatController.getThreat);
+routes.get("/threats", ThreatController.getThreats);
+
 export default routes;
