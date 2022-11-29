2461e32751e030f6c3e039749918aa6bf8321ca0
Othavio Henrico <46772924+OthavioH@users.noreply.github.com>
2022-11-29 18:41:47 -0300

    implemented certificate logic

--- dist/app.js
+++ dist/app.js
@@ -7,7 +7,9 @@ exports.socketController = void 0;
 const cors_1 = __importDefault(require("cors"));
 const express_1 = __importDefault(require("express"));
 const morgan_1 = __importDefault(require("morgan"));
-const http_1 = __importDefault(require("http"));
+const https_1 = __importDefault(require("https"));
+const path_1 = __importDefault(require("path"));
+const fs_1 = __importDefault(require("fs"));
 const routes_1 = __importDefault(require("./routers/routes"));
 const data_source_1 = require("./config/data-source");
 const SocketController_1 = require("./controllers/SocketController");
@@ -19,7 +21,10 @@ data_source_1.AppDataSource.initialize().then(() => {
     app.use(express_1.default.urlencoded({ extended: true, limit: '500mb' }));
     app.use((0, morgan_1.default)('dev'));
     app.use(routes_1.default);
-    const server = http_1.default.createServer(app);
+    const server = https_1.default.createServer({
+        key: fs_1.default.readFileSync(path_1.default.join(__dirname, 'cert', 'key.pem')),
+        cert: fs_1.default.readFileSync(path_1.default.join(__dirname, 'cert', 'cert.pem')),
+    }, app);
     exports.socketController.initialize(server);
-    server.listen(process.env.PORT || 3333, () => console.log(`Server started on port ${process.env.PORT || 3333}`));
+    server.listen(process.env.PORT || 3443, () => console.log(`Server started on port ${process.env.PORT || 3443}`));
 });
